9261d49e7d871d0776eee0bbfba22fb8d17b2759
Андрей Теплицын <ateplitsin@intro.local>
2025-05-23 15:13:22 +0700

    made up the layout fix 0.1

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,9 +1,7 @@
 import { CardsList } from "./cards-list";
 
-export const Home = () => {
-  return (
-    <section className="mx-auto my-0 mt-10 max-w-5xl">
-      <CardsList />
-    </section>
-  );
-};
+export const Home = () => (
+  <section className="mx-auto my-0 mt-10 max-w-5xl">
+    <CardsList />
+  </section>
+);
--- src/components/product-details/index.tsx
+++ src/components/product-details/index.tsx
@@ -1,22 +1,20 @@
 import barry from "../../assets/image/berry.jpeg";
 
-export const ProductDetails = () => {
-  return (
-    <div className="mx-auto flex max-w-4xl gap-15">
-      <div className="overflow-hidden rounded-4xl border-2">
-        <img src={barry} width={400} height={400} alt="Товар" />
-      </div>
-      <div className="flex flex-col h-100%">
-        <h3 className="text-4xl mb-5">Название товара</h3>
-        <p className="mb-5">категория</p>
-        <p>Описание</p>
-        <button
-          className="mt-auto cursor-pointer rounded-2xl border-2 bg-orange-400 px-8 py-3"
-          type="button"
-        >
-          Добавить в корзину
-        </button>
-      </div>
+export const ProductDetails = () => (
+  <div className="mx-auto flex max-w-4xl gap-15">
+    <div className="overflow-hidden rounded-4xl border-2">
+      <img src={barry} width={400} height={400} alt="Товар" />
     </div>
-  );
-};
+    <div className="h-100% flex flex-col">
+      <h3 className="mb-5 text-4xl">Название товара</h3>
+      <p className="mb-5">категория</p>
+      <p>Описание</p>
+      <button
+        className="mt-auto cursor-pointer rounded-2xl border-2 bg-orange-400 px-8 py-3"
+        type="button"
+      >
+        Добавить в корзину
+      </button>
+    </div>
+  </div>
+);
